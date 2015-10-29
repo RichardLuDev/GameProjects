7287f250990fd6aae766f6417ef761b728d6a106
RichardLuDev <richard.lu.dev@gmail.com>
2015-10-28 21:55:32 -0400

    Added more functionality to Matrix class.

--- Utilities/src/richardludev/utilities/matrices/Matrix.java
+++ Utilities/src/richardludev/utilities/matrices/Matrix.java
@@ -5,14 +5,6 @@ public class Matrix {
     private final double[][] data;
     private final int m, n;
     
-    public static Matrix identity(int n){
-        Matrix I = new Matrix(n, n);
-        for(int i = 0; i < n; i++){
-            I.data[i][i] = 1;
-        }
-        return I;
-    }
-    
     public Matrix(int m, int n){
         this.m = m;
         this.n = n;
@@ -20,7 +12,7 @@ public class Matrix {
     }
     
     public Matrix(double[][] data){
-        if(data.length <= 0) throw new IllegalArgumentException("The 2D array has no data.");
+        if(data == null || data.length <= 0) throw new IllegalArgumentException("The 2D array has no data.");
         
         this.m = data.length;
         this.n = data[0].length;
@@ -34,23 +26,32 @@ public class Matrix {
         }
     }
     
-    public int getRowNum(){
+    public int getNumOfRows(){
         return m;
     }
     
-    public int getColNum(){
+    public int getNumOfCols(){
         return n;
     }
     
     public double getElement(int m, int n){
-        return this.data[m-1][n-1];
+        return this.data[m][n];
+    }
+    
+    public double[][] getData(){
+        double[][] outputData = new double[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++)
+                outputData[i][j] = this.data[i][j];
+        }
+        return outputData;
     }
     
     public Matrix add(Matrix B){
         Matrix A = this;
         if(A.m != B.m || A.n != B.n) throw new IllegalArgumentException("The matrices are not the same size");
         
-        Matrix C = new Matrix(this.m, this.n);
+        Matrix C = new Matrix(A.m, A.n);
         for(int i = 0; i < A.m; i++){
             for(int j = 0; j < A.n; j++){
                 C.data[i][j] = A.data[i][j] + B.data[i][j];
@@ -63,12 +64,121 @@ public class Matrix {
         Matrix A = this;
         if(A.m != B.m || A.n != B.n) throw new IllegalArgumentException("The matrices are not the same size");
         
-        Matrix C = new Matrix(this.m, this.n);
+        Matrix C = new Matrix(A.m, A.n);
         for(int i = 0; i < A.m; i++){
             for(int j = 0; j < A.n; j++){
                 C.data[i][j] = A.data[i][j] - B.data[i][j];
             }
         }
         return C;
+    }
+    
+    public Matrix multiply(Matrix B){
+        Matrix A = this;
+        if(A.n != B.m) throw new IllegalArgumentException("The matrices with the given sizes cannot be multiplied.");
+        
+        Matrix C = new Matrix(A.m, B.n);
+        for(int i=0; i < C.m; i++){
+            for(int j=0; j < C.n; j++){
+                for(int k=0; k < A.n; k++){
+                    C.data[i][j] += A.data[i][k]*B.data[k][j];
+                }
+            }
+        }
+        return C;
+    }
+    
+    public boolean Equals(Matrix other){
+        if(this.m != other.m) return false;
+        if(this.n != other.n) return false;
+        
+        for(int i = 0; i < this.m; i++){
+            for(int j = 0; j < this.n; j++){
+                if(this.data[i][j] != other.data[i][j]) return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean Equals(Matrix other, double tolerance){
+        if(this.m != other.m) return false;
+        if(this.n != other.n) return false;
+        
+        for(int i = 0; i < this.m; i++){
+            for(int j = 0; j < this.n; j++){
+                if(Math.abs(this.data[i][j] - other.data[i][j]) > tolerance) return false;
+            }
+        }
+        return true;
+    }
+    
+    public static Matrix columnVector(double[] data){
+        if(data.length <= 0) throw new IllegalArgumentException("The array has no data.");
+        
+        Matrix V = new Matrix(data.length, 1);
+        for(int i=0; i < data.length; i++){
+            V.data[i][0] = data[i];
+        }
+        return V;
+    }
+    
+    public static Matrix rowVector(double[] data){
+        if(data.length <= 0) throw new IllegalArgumentException("The array has no data.");
+        
+        Matrix V = new Matrix(1, data.length);
+        for(int j=0; j < data.length; j++){
+            V.data[0][j] = data[j];
+        }
+        return V;
+    }
+    
+    public static Matrix identity(int n){      
+        Matrix I = new Matrix(n, n);
+        for(int i = 0; i < n; i++){
+            I.data[i][i] = 1;
+        }
+        return I;
+    }
+    
+    public static Matrix rotational(double theta){
+        Matrix R = identity(3);
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+        R.data[0][0] = cosTheta;
+        R.data[0][1] = -sinTheta;
+        R.data[1][0] = sinTheta;
+        R.data[1][1] = cosTheta;
+        
+        return R;
+    }
+    
+    public static Matrix rotational(double theta, double x, double y){
+        Matrix R = identity(3);
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+        R.data[0][0] = cosTheta;
+        R.data[0][1] = -sinTheta;
+        R.data[0][2] = x - cosTheta*x + sinTheta*y;
+        R.data[1][0] = sinTheta;
+        R.data[1][1] = cosTheta;
+        R.data[1][2] = y - sinTheta*x - cosTheta*y;
+        
+        return R;
+    }
+    
+    public static Matrix translational(double x, double y){
+        Matrix T = identity(3);
+        T.data[0][2] = x;
+        T.data[1][2] = y;
+        
+        return T;
+    }
+    
+    public static Matrix scaling(double tx, double ty){
+        Matrix S = identity(3);
+        S.data[0][0] = tx;
+        S.data[1][1] = ty;
+        
+        return S;
     }
 }
--- Utilities/test/richardludev/utilities/matrices/MatrixTest.java
+++ Utilities/test/richardludev/utilities/matrices/MatrixTest.java
@@ -9,8 +9,8 @@ public class MatrixTest {
     
     private static final double EPSILON = 10E-4;
     
-    private double[][] data3x4A, data3x4B, data3x3, dataInvalid, dataEmpty;
-    private Matrix matrix1, matrix2;
+    private double[][] data3x4A, data3x4B, data3x3, data4x3, data3x4A_times_data4x3, dataInvalid, dataEmpty;
+    private double[] dataVector3, dataVectorEmpty;
     
     @Before
     public void setUp(){
@@ -32,6 +32,19 @@ public class MatrixTest {
                 {7, 8, 9}
         };
         
+        data4x3 = new double[][]{
+                {2, 4, 6},
+                {8, 10, 12},
+                {14, 16, 18},
+                {20, 22, 24}
+        };
+        
+        data3x4A_times_data4x3 = new double[][]{
+                {140, 160, 180},
+                {316, 368, 420},
+                {492, 576, 660},
+        };
+        
         dataInvalid = new double[][]{
                 {2, 2, 2, 2, 2},
                 {2, 2, 2, 2},
@@ -39,6 +52,10 @@ public class MatrixTest {
         };
         
         dataEmpty = new double[0][0];
+        
+        dataVector3 = new double[] {10, 20, 1};
+        
+        dataVectorEmpty = new double[0];
     }
     
     @Test(expected = IllegalArgumentException.class)
@@ -55,33 +72,12 @@ public class MatrixTest {
     public void constructor_goodData(){
         Matrix output = new Matrix(data3x4A);
         
-        Assert.assertEquals("Unexpected number of rows.", output.getRowNum(), data3x4A.length);
-        Assert.assertEquals("Unexpected number of columns.", output.getColNum(), data3x4A[0].length);
+        Assert.assertEquals("Unexpected number of rows.", output.getNumOfRows(), data3x4A.length);
+        Assert.assertEquals("Unexpected number of columns.", output.getNumOfCols(), data3x4A[0].length);
         
-        for(int i = 1; i <= output.getRowNum(); i++){
-            for(int j = 1; j <= output.getColNum(); j++){
-                Assert.assertEquals("Unexpected value.", data3x4A[i-1][j-1], output.getElement(i, j), 0);
-            }
-        }
-    }
-    
-    @Test
-    public void identity_goodData(){
-        int size = 3;
-        double diagonalValue = 1;
-        double nondiagonalValue = 0;
-        
-        Matrix output = Matrix.identity(size);
-        
-        Assert.assertEquals("Unexpected size.", output.getRowNum(), size);
-        Assert.assertEquals("Unexpected size.", output.getColNum(), size);
-        
-        for(int i = 1; i <= output.getRowNum(); i++){
-            for(int j = 1; j <= output.getColNum(); j++){
-                if(i == j)
-                    Assert.assertEquals("Unexpected diagonal value.", output.getElement(i, j), diagonalValue, 0);
-                if(i != j)
-                    Assert.assertEquals("Unexpected non-diagonal value.", output.getElement(i, j), nondiagonalValue, 0);
+        for(int i = 0; i < output.getNumOfRows(); i++){
+            for(int j = 0; j < output.getNumOfCols(); j++){
+                Assert.assertEquals("Unexpected value.", data3x4A[i][j], output.getElement(i, j), 0);
             }
         }
     }
@@ -101,11 +97,11 @@ public class MatrixTest {
         
         Matrix output = matrix3x4A.add(matrix3x4B);
         
-        Assert.assertEquals("Unexpected size.", matrix3x4A.getRowNum(), output.getRowNum());
-        Assert.assertEquals("Unexpected size.", matrix3x4A.getColNum(), output.getColNum());
+        Assert.assertEquals("Unexpected size.", matrix3x4A.getNumOfRows(), output.getNumOfRows());
+        Assert.assertEquals("Unexpected size.", matrix3x4A.getNumOfCols(), output.getNumOfCols());
         
-        for(int i = 1; i < output.getRowNum(); i++){
-            for(int j = 1; j < output.getColNum(); j++){
+        for(int i = 0; i < output.getNumOfRows(); i++){
+            for(int j = 0; j < output.getNumOfCols(); j++){
                 Assert.assertEquals(matrix3x4A.getElement(i, j) + matrix3x4B.getElement(i, j), output.getElement(i, j), EPSILON);
             }
         }
@@ -126,13 +122,183 @@ public class MatrixTest {
         
         Matrix output = matrix3x4A.subtract(matrix3x4B);
         
-        Assert.assertEquals("Unexpected size.", matrix3x4A.getRowNum(), output.getRowNum());
-        Assert.assertEquals("Unexpected size.", matrix3x4A.getColNum(), output.getColNum());
+        Assert.assertEquals("Unexpected size.", matrix3x4A.getNumOfRows(), output.getNumOfRows());
+        Assert.assertEquals("Unexpected size.", matrix3x4A.getNumOfCols(), output.getNumOfCols());
         
-        for(int i = 1; i < output.getRowNum(); i++){
-            for(int j = 1; j < output.getColNum(); j++){
+        for(int i = 0; i < output.getNumOfRows(); i++){
+            for(int j = 1; j < output.getNumOfCols(); j++){
                 Assert.assertEquals(matrix3x4A.getElement(i, j) - matrix3x4B.getElement(i, j), output.getElement(i, j), EPSILON);
             }
+        }
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void mutiply_incompatibleSize(){
+        Matrix matrix3x4A = new Matrix(data3x4A);
+        Matrix matrix3x3 = new Matrix(data3x3);
+        
+        matrix3x4A.multiply(matrix3x3);
+    }
+    
+    @Test
+    public void mutiply_goodData(){
+        Matrix matrix3x4A = new Matrix(data3x4A);
+        Matrix matrix4x3 = new Matrix(data4x3);
+        Matrix expectedOutput = new Matrix(data3x4A_times_data4x3);
+        
+        Matrix output = matrix3x4A.multiply(matrix4x3);
+        
+        Assert.assertEquals("Unexpected size.", matrix3x4A.getNumOfRows(), output.getNumOfRows());
+        Assert.assertEquals("Unexpected size.", matrix4x3.getNumOfCols(), output.getNumOfCols());
+        
+        for(int i = 1; i < output.getNumOfRows(); i++){
+            for(int j = 1; j < output.getNumOfCols(); j++){
+                Assert.assertEquals(expectedOutput.getElement(i, j), output.getElement(i, j), EPSILON);
+            }
+        }
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void columnVector_noData(){        
+        Matrix.columnVector(dataVectorEmpty);
+    }
+    
+    @Test
+    public void columnVector_sizeThree(){        
+        Matrix output = Matrix.columnVector(dataVector3);
+        
+        double[][] expectedData = {
+                {dataVector3[0]},
+                {dataVector3[1]}, 
+                {dataVector3[2]}};
+        Matrix expected = new Matrix(expectedData);
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void rowVector_noData(){        
+        Matrix.rowVector(dataVectorEmpty);
+    }
+    
+    @Test
+    public void rowVector_sizeThree(){        
+        Matrix output = Matrix.rowVector(dataVector3);
+        
+        double[][] expectedData = {
+                {dataVector3[0], dataVector3[1], dataVector3[2]}};
+        Matrix expected = new Matrix(expectedData);
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test
+    public void identity_sizeThree(){
+        int size = 3;
+        
+        Matrix output = Matrix.identity(size);
+        
+        double[][] expectedData = {
+                {1, 0, 0},
+                {0, 1, 0}, 
+                {0, 0, 1}};
+        Matrix expected = new Matrix(expectedData);
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test
+    public void identity_sizeThreeTransformVector(){
+        Matrix vector = Matrix.columnVector(dataVector3);
+        Matrix identity = Matrix.identity(dataVector3.length);
+        
+        Matrix output = identity.multiply(vector);
+        
+        Assert.assertTrue(output.Equals(vector, EPSILON));
+    }
+    
+    @Test
+    public void rotational_positiveAngle(){
+        double angle = 1;
+        Matrix output = Matrix.rotational(angle);
+        
+        double cosAngle = Math.cos(angle);
+        double sinAngle = Math.sin(angle);
+        
+        double[][] expectedData = {
+                {cosAngle, -sinAngle, 0},
+                {sinAngle, cosAngle, 0}, 
+                {0, 0, 1}};
+        Matrix expected = new Matrix(expectedData);
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test
+    public void rotational_negativeAngle(){
+        double angle = -2;
+        Matrix output = Matrix.rotational(angle);
+        
+        double cosAngle = Math.cos(angle);
+        double sinAngle = Math.sin(angle);
+        
+        double[][] expectedData = {
+                {cosAngle, -sinAngle, 0},
+                {sinAngle, cosAngle, 0}, 
+                {0, 0, 1}};
+        Matrix expected = new Matrix(expectedData);
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test
+    public void rotational_positiveAngleTransformVector(){
+        double angle = 1;
+        Matrix vector = Matrix.columnVector(dataVector3);
+        Matrix rotational = Matrix.rotational(angle);
+        
+        Matrix output = rotational.multiply(vector);
+        
+        Matrix expected = Matrix.columnVector(new double[] {-11.426, 19.221, 1});
+        Assert.assertTrue(output.Equals(expected, EPSILON));
+    }
+    
+    @Test
+    public void rotational_positiveAngleAboutPoint(){
+        double angle = 0.5;
+        double dx = 3;
+        double dy = 4.5;
+        
+        Matrix output = Matrix.rotational(angle, dx, dy);
+        double[][] outputData = output.getData();
+        
+        double cosAngle = Math.cos(angle);
+        double sinAngle = Math.sin(angle);
+        
+        double[][] expectedData = {
+                {cosAngle, -sinAngle, dx - cosAngle*dx + sinAngle*dy},
+                {sinAngle, cosAngle, dy - sinAngle*dx - cosAngle*dy}, 
+                {0, 0, 1}};
+        
+        for(int i = 0; i < expectedData.length; i++){
+            Assert.assertArrayEquals(expectedData[i], outputData[i], EPSILON);
+        }
+    }
+    
+    @Test
+    public void rotational_negativeAngleAboutPoint(){
+        double angle = -1.5;
+        double dx = 3;
+        double dy = 4.5;
+        
+        Matrix output = Matrix.rotational(angle, dx, dy);
+        double[][] outputData = output.getData();
+        
+        double cosAngle = Math.cos(angle);
+        double sinAngle = Math.sin(angle);
+        
+        double[][] expectedData = {
+                {cosAngle, -sinAngle, dx - cosAngle*dx + sinAngle*dy},
+                {sinAngle, cosAngle, dy - sinAngle*dx - cosAngle*dy}, 
+                {0, 0, 1}};
+        
+        for(int i = 0; i < expectedData.length; i++){
+            Assert.assertArrayEquals(expectedData[i], outputData[i], EPSILON);
         }
     }
 }
